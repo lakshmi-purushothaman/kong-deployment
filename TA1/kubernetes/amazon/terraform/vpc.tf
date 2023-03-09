08f76b69170eb57ae1cfd702c7b7b216001056e7
Lakshmi Purushotham <lakshmi@Lakshmis-MacBook-Air.local>
2023-03-09 11:53:45 +0000

    VPC version update

--- TA1/kubernetes/amazon/terraform/vpc.tf
+++ TA1/kubernetes/amazon/terraform/vpc.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
+  version = "3.19.0"
 
   name            = var.vpc_name
   cidr            = "10.0.0.0/16"
